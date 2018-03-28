e134f39076237acf3b6b6520ad5d7e283189aa0c
rafageist <cubapush@gmail.com>
2018-03-28 16:15:12 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,13 @@ $(function(){
         $("#memory-images").modal();
     });
     
+    // contact me
+    $("#contact-me").click(function(){
+        $("#contact-me-form").modal();
+    });
+    
+    $("#contact-send").click(function(){
+       $("#form-contact-me").submit();
+       $("#contact-me-form").modal('hide'); 
+    });
 });
