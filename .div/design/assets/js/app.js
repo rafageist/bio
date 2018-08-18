25f226b069864917488042b3c6262ea41b475094
rafageist <cubapush@gmail.com>
2018-08-18 14:26:11 -0400

    update

---
+++ .div/design/assets/js/app.js
@@ -0,0 +1,21 @@
+$(function(){
+   
+    // memory images
+    $("#show-memory-images").click(function(){
+        $("#memory-images").modal();
+    });
+    
+    // contact me
+    $("#contact-me").click(function(){
+        $("#contact-me-form").modal();
+    });
+    
+    $("#contact-send").click(function(){
+       $("#form-contact-me").submit();
+       $("#contact-me-form").modal('hide'); 
+    });
+    
+    $("#show-esd-10").click(function(){
+        $("#esd-10").modal();
+    })
+});
--- build.sh
+++ build.sh
@@ -1,2 +1,36 @@
-div @inject -ff index.html -fb "<div>--</div>" -tf ./.div/index.tpl -tb "<!--{imported}-->"
-div @copy ./.div/index.tpl ./index.html
+#!/bin/sh
+
+# Re-creating index.html
+rm -f ./index.html
+cp ./.div/design/index.html ./index.html
+
+# Inject google analytics
+div @inject -ff ./.div/pieces/google-analytics.html -fb "<!--{analytics}-->" -tf ./index.html -ta "<head>"
+
+# Re-creating assets
+rm -f -R ./assets
+rm -f -R ./assets/css
+rm -f -R ./assets/js
+rm -f -R ./assets/fonts
+rm -f -R ./assets/bootstrap
+
+div @mkdir ./assets/css
+div @mkdir ./assets/js
+div @mkdir ./assets/fonts
+div @mkdir ./assets/img
+div @mkdir ./assets/bootstrap/css
+div @mkdir ./assets/bootstrap/js
+div @mkdir ./assets/bootstrap/fonts
+
+cp -R ./.div/design/assets/*.* ./assets
+cp -R ./.div/design/assets/css/*.* ./assets/css
+cp -R ./.div/design/assets/js/*.* ./assets/js
+cp -R ./.div/design/assets/fonts/*.* ./assets/fonts
+cp -R ./.div/design/assets/img/*.* ./assets/img
+
+cp -R ./.div/design/assets/bootstrap/css/*.* ./assets/bootstrap/css
+cp -R ./.div/design/assets/bootstrap/fonts/*.* ./assets/bootstrap/fonts
+cp -R ./.div/design/assets/bootstrap/js/*.* ./assets/bootstrap/js
+
+# Clean
+rm ./index.html.bak
