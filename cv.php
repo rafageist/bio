2f82e483e4fb6caf0582f9fbe16079c3a2bd5bc9
rafageist <rafageist@hotmail.com>
2021-06-30 22:07:20 -0300

    update

--- cv.php
+++ cv.php
@@ -6,6 +6,22 @@ include "div.php";
 
 $serial = strtoupper(uniqid());
 
-div::setGlobal("serial", $serial);
+$ip = false;
 
-echo new div("rafageist.tpl", "rafageist.json");
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+div::setGlobal("serial", $serial);
+div::setGlobal("ip", $ip);
+
+$tpl = (new div("rafageist.tpl", "rafageist.json"))."";
+
+
+file_put_contents("/var/www/cv/$serial-".date('Ymdhis').".html", $tpl);
+
+echo $tpl;
